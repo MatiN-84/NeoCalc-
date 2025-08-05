7f2316fd218df55edeeca579b47c0b33f7a76be9
Matin Ansaripour <matin.ansari.pour.84@gmail.com>
2025-08-05 21:45:39 +0330

    feat(switchButton  styles): theme2 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "./components/Button";
 import buttons from "./constants/buttons.js"
 import SwitchButton from "./components/switchButton.js";
@@ -6,29 +6,38 @@ import styles from "./App.module.css"
 function App() {
   const [theme, setTheme] = useState("1");
   const [text, setText] = useState("")
+
+  const mainColors = ["hsl(222, 26%, 31%)" , "hsl(0, 0%, 90%)" , "hsl(268, 75%, 9%)"]
+  useEffect(()=> {
+    
+    document.body.style.backgroundColor = mainColors[theme-1];
+
+
+  }, [theme])
   return (
-    <div className={styles.body}>
+
+
           <div className={styles.calculator}>
-      <div className={styles.header}>
+      <div className={`${styles.header} ${styles["header"+theme]}`}>
         <span>calc</span>
-        <div className={styles.switch} >
+        <div className={`${styles.switch} ${styles["switch"+theme]}`} >
           <span>Theme</span>
           <SwitchButton theme={theme} setTheme={setTheme} />
         </div>
       </div>
 
 
-      <div className={styles.screen}>
+      <div className={`${styles.screen} ${styles["screen"+theme]}`}>
         {text}
       </div>
 
-      <div className={styles.buttonContainer}  >
+      <div className={`${styles.buttonContainer} ${styles["buttonContainer"+theme]}`}  >
         {buttons.map((button) => (
           <Button theme={theme} key={button.character} button={button} text={text} setText={setText}  />
         ))}
       </div>
     </div>
-    </div>
+
 
   );
 }
--- src/components/SwitchButton.tsx
+++ src/components/SwitchButton.tsx
@@ -3,10 +3,10 @@ import styles from "./SwitchButton.module.css";
 function SwitchButton({theme , setTheme}) {
     
   return (
-    <div className={`${styles.switchButton}`}>
+    <div className={`${styles.switchButton} ${styles["switchButton"+theme]}`}>
 
-      <div className={styles.numbers}>{"1   2   3"}</div>
-      <div className={styles.button}>
+      <div className={`${styles.numbers} ${styles["numbers"+theme]}`}>{"1   2   3"}</div>
+      <div className={`${styles.button} ${styles["button"+theme]}`}>
         <div className={`${styles.circle} ${styles['circle' + theme]}`}></div>
         <div className={`${styles.section1} ${styles.section}`} onClick={() => setTheme("1")}></div>
         <div className={`${styles.section2} ${styles.section}`} onClick={() => setTheme("2")}></div>
